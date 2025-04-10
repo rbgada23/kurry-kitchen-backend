86fb62ec3da2dd719964e04ed09c62f0abfe6b9c
Rishabh Gada <32638942+rbgada23@users.noreply.github.com>
2025-04-10 15:32:59 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const io = socketIO(server, {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:3000", "https://kurrykitchen-1f3cc.web.app"],
+  origin: ["http://localhost:3000", "https://kurrykitchen-1f3cc.web.app","https://mealmate-ded01.web.app"],
   credentials: true, // Allow cookies
   allowedHeaders: ["Content-Type", "Authorization"],
   methods: ["GET", "POST", "PUT", "DELETE"],
